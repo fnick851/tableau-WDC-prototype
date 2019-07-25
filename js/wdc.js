6a6a72c7beb946dfa1e8b926fe3cd28e33908241
(Noah) Hanlun Song <hsong@air.org>
2019-07-25 13:52:22 -0400

    Add in query input field

--- js/wdc.js
+++ js/wdc.js
@@ -3,8 +3,6 @@
     // HTTP call for API
     // ---
     const postApiUrl = "http://54.175.11.175:8080/api/v1/analysis?async=false"
-    const getApiUrl =
-        'http://54.175.11.175:8080/api/v1/analysis?spec={"scope":{"limit":"unlimited","selector":{"type":"byQuery","query":"wireless device","queryParser":""}},"labels":{"maxLabels":100,"source":{"fields":[{"name":"patent_title$phrases","weight":1},{"name":"patent_abstract$phrases","weight":1}]},"surface":{"minWordCount":1,"maxWordCount":8,"minCharacterCount":4,"minWordCharacterCountAverage":2.9,"preferredWordCount":2.5,"preferredWordCountDeviation":2.5,"singleWordLabelWeightMultiplier":0.5,"capitalizedLabelWeight":1,"uppercaseLabelWeight":1,"acronymLabelWeight":1,"exclude":[{"type":"project","dictionary":"default"}]},"frequencies":{"minAbsoluteDf":2,"minRelativeDf":0,"maxRelativeDf":0.4,"maxLabelsPerDocument":10,"truncatedPhraseThreshold":0.2},"probabilities":{"autoStopLabelRemovalStrength":0.35,"autoStopLabelMinCoverage":0.4},"scorers":{"tokenCountScorerWeight":1,"tfScorerWeight":1,"idfScorerWeight":1,"completePhrasesScorerWeight":1,"truncatedPhrasesScorerWeight":1,"dictionaryScorerWeight":1,"tokenCaseScorerWeight":1},"arrangement":{"enabled":false,"algorithm":{"type":"ap","ap":{"maxIterations":2000,"minSteadyIterations":100,"threads":"auto","softening":0.9,"damping":0.9,"minPruningGain":0.3,"inputPreference":0,"preferenceInitializer":"NONE","preferenceInitializerScaling":1}},"relationship":{"type":"cooccurrences","cooccurrences":{"cooccurrenceWindowSize":32,"cooccurrenceCountingAccuracy":0.5,"similarityWeighting":"INCLUSION","threads":"auto"}}},"direct":[]},"documents":{"arrangement":{"enabled":true,"algorithm":{"type":"ap","ap":{"maxIterations":2000,"minSteadyIterations":100,"threads":"8","softening":0.9,"damping":0.9,"minPruningGain":0.3,"inputPreference":0,"addSelfSimilarityToPreference":false},"maxClusterLabels":3,"maxLabelsPerDocument":10},"relationship":{"type":"mlt","mlt":{"maxSimilarDocuments":8,"minDocumentLabels":1,"maxQueryLabels":4,"minQueryLabelOccurrences":1,"minMatchingQueryLabels":1,"maxScopeSizeForSubIndex":0.3,"maxInMemorySubIndexSize":8000000,"threads":"16"}}},"embedding":{"enabled":true,"algorithm":{"type":"lv","lv":{"maxIterations":300,"negativeEdgeCount":5,"negativeEdgeWeight":2,"negativeEdgeDenominator":1,"threads":"16"}},"relationship":{"type":"mlt","mlt":{"maxSimilarDocuments":8,"minDocumentLabels":1,"maxQueryLabels":4,"minQueryLabelOccurrences":1,"minMatchingQueryLabels":1,"maxScopeSizeForSubIndex":0.3,"maxInMemorySubIndexSize":8000000,"threads":"16","maxSimilarDocumentsPerLabel":5}}}},"output":{"format":"json","parameters":{},"pretty":false,"labels":{"enabled":true,"labelFormat":"ORIGINAL","documents":{"enabled":false,"maxDocumentsPerLabel":10,"outputScores":false}},"documents":{"enabled":false,"onlyWithLabels":true,"onlyAssignedToLabels":false,"labels":{"enabled":false,"maxLabelsPerDocument":2147483647,"minLabelOccurrencesPerDocument":0},"content":{"enabled":false,"fields":[]}}},"performance":{"threads":"16"},"summary":{"labeledDocuments":true},"debug":{"logCandidateLabelPartialScores":false}}&async=false'
     const postData = {
         scope: {
             limit: "unlimited",
@@ -198,39 +196,9 @@
             "Cache-Control": "no-cache"
         }
     }
-    const tableData = []
-    axios
-        .get(
-            // postApiUrl,
-            getApiUrl
-            // postData,
-            // postHeader,
-        )
-        .then(function(response) {
-            console.log("response data: ", response.data)
-            const labelsEmbedPos = response.data.documents.embedding.labels
-            const labels = response.data.labels.list
 
-            for (var i = 0; i < labelsEmbedPos.length; i++) {
-                tableData.push({
-                    documents_embedding_labels_id: labelsEmbedPos[i].id,
-                    documents_embedding_labels_x: labelsEmbedPos[i].x,
-                    documents_embedding_labels_y: labelsEmbedPos[i].y,
-                    labels_list_id: labels[i].id,
-                    labels_list_display: labels[i].display,
-                    labels_list_text: labels[i].text,
-                    labels_list_score: labels[i].score,
-                    labels_list_df: labels[i].df
-                })
-            }
-
-            console.log("filtered data: ", tableData)
-        })
-        .catch(function(error) {
-            console.log(error)
-        })
     // ---
-    // Initiate WDC
+    // Set up WDC
     // ---
     const wdcConnector = tableau.makeConnector()
 
@@ -281,13 +249,51 @@
     }
 
     wdcConnector.getData = function(table, doneCallback) {
-        table.appendRows(tableData)
-        doneCallback()
+        const search_query = document.querySelector("#search_query").value
+        const getApiUrl =
+            'http://54.175.11.175:8080/api/v1/analysis?spec={"scope":{"limit":"unlimited","selector":{"type":"byQuery","query":"' +
+            search_query +
+            '","queryParser":""}},"labels":{"maxLabels":100,"source":{"fields":[{"name":"patent_title$phrases","weight":1},{"name":"patent_abstract$phrases","weight":1}]},"surface":{"minWordCount":1,"maxWordCount":8,"minCharacterCount":4,"minWordCharacterCountAverage":2.9,"preferredWordCount":2.5,"preferredWordCountDeviation":2.5,"singleWordLabelWeightMultiplier":0.5,"capitalizedLabelWeight":1,"uppercaseLabelWeight":1,"acronymLabelWeight":1,"exclude":[{"type":"project","dictionary":"default"}]},"frequencies":{"minAbsoluteDf":2,"minRelativeDf":0,"maxRelativeDf":0.4,"maxLabelsPerDocument":10,"truncatedPhraseThreshold":0.2},"probabilities":{"autoStopLabelRemovalStrength":0.35,"autoStopLabelMinCoverage":0.4},"scorers":{"tokenCountScorerWeight":1,"tfScorerWeight":1,"idfScorerWeight":1,"completePhrasesScorerWeight":1,"truncatedPhrasesScorerWeight":1,"dictionaryScorerWeight":1,"tokenCaseScorerWeight":1},"arrangement":{"enabled":false,"algorithm":{"type":"ap","ap":{"maxIterations":2000,"minSteadyIterations":100,"threads":"auto","softening":0.9,"damping":0.9,"minPruningGain":0.3,"inputPreference":0,"preferenceInitializer":"NONE","preferenceInitializerScaling":1}},"relationship":{"type":"cooccurrences","cooccurrences":{"cooccurrenceWindowSize":32,"cooccurrenceCountingAccuracy":0.5,"similarityWeighting":"INCLUSION","threads":"auto"}}},"direct":[]},"documents":{"arrangement":{"enabled":true,"algorithm":{"type":"ap","ap":{"maxIterations":2000,"minSteadyIterations":100,"threads":"8","softening":0.9,"damping":0.9,"minPruningGain":0.3,"inputPreference":0,"addSelfSimilarityToPreference":false},"maxClusterLabels":3,"maxLabelsPerDocument":10},"relationship":{"type":"mlt","mlt":{"maxSimilarDocuments":8,"minDocumentLabels":1,"maxQueryLabels":4,"minQueryLabelOccurrences":1,"minMatchingQueryLabels":1,"maxScopeSizeForSubIndex":0.3,"maxInMemorySubIndexSize":8000000,"threads":"16"}}},"embedding":{"enabled":true,"algorithm":{"type":"lv","lv":{"maxIterations":300,"negativeEdgeCount":5,"negativeEdgeWeight":2,"negativeEdgeDenominator":1,"threads":"16"}},"relationship":{"type":"mlt","mlt":{"maxSimilarDocuments":8,"minDocumentLabels":1,"maxQueryLabels":4,"minQueryLabelOccurrences":1,"minMatchingQueryLabels":1,"maxScopeSizeForSubIndex":0.3,"maxInMemorySubIndexSize":8000000,"threads":"16","maxSimilarDocumentsPerLabel":5}}}},"output":{"format":"json","parameters":{},"pretty":false,"labels":{"enabled":true,"labelFormat":"ORIGINAL","documents":{"enabled":false,"maxDocumentsPerLabel":10,"outputScores":false}},"documents":{"enabled":false,"onlyWithLabels":true,"onlyAssignedToLabels":false,"labels":{"enabled":false,"maxLabelsPerDocument":2147483647,"minLabelOccurrencesPerDocument":0},"content":{"enabled":false,"fields":[]}}},"performance":{"threads":"16"},"summary":{"labeledDocuments":true},"debug":{"logCandidateLabelPartialScores":false}}&async=false'
+        const tableData = []
+
+        axios
+            .get(
+                // postApiUrl,
+                getApiUrl
+                // postData,
+                // postHeader,
+            )
+            .then(function(response) {
+                console.log("response data: ", response.data)
+                const labelsEmbedPos = response.data.documents.embedding.labels
+                const labels = response.data.labels.list
+
+                for (var i = 0; i < labelsEmbedPos.length; i++) {
+                    tableData.push({
+                        documents_embedding_labels_id: labelsEmbedPos[i].id,
+                        documents_embedding_labels_x: labelsEmbedPos[i].x,
+                        documents_embedding_labels_y: labelsEmbedPos[i].y,
+                        labels_list_id: labels[i].id,
+                        labels_list_display: labels[i].display,
+                        labels_list_text: labels[i].text,
+                        labels_list_score: labels[i].score,
+                        labels_list_df: labels[i].df
+                    })
+                }
+
+                console.log("filtered data: ", tableData)
+
+                table.appendRows(tableData)
+                doneCallback()
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
     }
 
     tableau.registerConnector(wdcConnector)
 
-    const fetchButton = document.querySelector(".fetch_data_button")
+    const fetchButton = document.querySelector("#search_button")
 
     fetchButton.addEventListener("click", function() {
         tableau.connectionName = "Lingo4G Patents View Prototype WDC"
